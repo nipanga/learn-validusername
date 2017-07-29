e0dea10bcf6a1b95cae174559a32824b03acb413
Juliano Felipe <jotafelipe@gmail.com>
2017-07-29 00:20:57 -0300

    Make UserEndpoint accept just one username at checkUsernames
    Makes the UserEndpoint accepts just one username at checkUsername
    method, instead of a list.
    
    Signed-off-by: Juliano Felipe <jotafelipe@gmail.com>

--- validusername/src/main/java/com/learn/springboot/validusername/endpoints/UserEndpoint.java
+++ validusername/src/main/java/com/learn/springboot/validusername/endpoints/UserEndpoint.java
@@ -5,19 +5,23 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
-import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.learn.springboot.validusername.dto.UserDTO;
 import com.learn.springboot.validusername.dto.UserResponseDTO;
 import com.learn.springboot.validusername.dto.UserResponseDTOList;
 import com.learn.springboot.validusername.services.UserService;
 
 /**
  * Provides information about Users
+ * 
+ * [ This endpoint is here so we can test with a app like Postman or Restman
+ * (chrome and opera, respectively), and see the list of suggested usernames
+ * easily ]
  * 
  * @author felipe
  *
@@ -33,24 +37,20 @@ public class UserEndpoint {
     /**
      * Checks if the given Username(s) are valid.
      * 
-     * @param usernames
-     *            a list of usernames to be checked.
+     * @param username
+     *            the usernames to be checked.
      * @return a {@link UserResponseDTOList} containg information about each given
      *         {@code username}
      */
     @RequestMapping(value = "/check-username", method = RequestMethod.POST,
             consumes = "application/json", produces = "application/json")
     @ResponseBody
-    public UserResponseDTOList checkUserNames(@RequestBody final List<String> usernames) {
+    public UserResponseDTOList checkUserName(@RequestBody final UserDTO username) {
 	final UserResponseDTOList response = new UserResponseDTOList();
 	final List<UserResponseDTO> userResponseDTOs = new ArrayList<>();
-	if (CollectionUtils.isNotEmpty(usernames)) {
-	    for (String username : usernames) {
-		UserResponseDTO singleResponse = getUserService().isValidUsername(username);
-		userResponseDTOs.add(singleResponse);
-	    }
-	    response.setUserResponseDTOs(userResponseDTOs);
-	}
+	UserResponseDTO singleResponse = getUserService().isValidUsername(username.getUsername());
+	userResponseDTOs.add(singleResponse);
+	response.setUserResponseDTOs(userResponseDTOs);
 	return response;
     }
 
